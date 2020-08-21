e24bd08ebe1fc01fbead03b52bede3cf5a5d2dde
Artem Konovalov <artemian000@gmail.com>
2020-08-22 00:18:12 +0700

    telegram validation realized

--- notifier/message_consumers/chat_types/telegram.py
+++ notifier/message_consumers/chat_types/telegram.py
@@ -1,8 +1,11 @@
 import asyncio
 import dataclasses
+import telebot
 from typing import List, Union, Dict
 
+from django.core.exceptions import ValidationError
 from django.db.models import JSONField
+from telebot.util import AsyncTask
 
 from helpers.messages_components import Message, CONSUMER_REGISTRY_NAME
 from helpers.registry import Registry
@@ -12,6 +15,10 @@ from .chats_base import Chat
 @Registry.register(CONSUMER_REGISTRY_NAME)
 @dataclasses.dataclass
 class TelegramGroupChat(Chat):
+    bot_token: str
+    chat_id: int
+
+    CHAT_NOT_FOUND = 'chat not found'
 
     @property
     def username_key(self):
@@ -19,7 +26,15 @@ class TelegramGroupChat(Chat):
 
     @classmethod
     def validate_params(cls, params: Union[Dict, JSONField]):
-        pass
+        bot = telebot.AsyncTeleBot(token=params['bot_token'])
+        chat_task: AsyncTask = bot.get_chat(params['chat_id'])
+        chat = chat_task.wait()
+        if not isinstance(chat, telebot.types.Chat):
+            exception = chat[1]
+            if cls.CHAT_NOT_FOUND in str(exception):
+                raise ValidationError(cls.CHAT_NOT_FOUND)
+
+            raise exception
 
     async def consume_messages(self, messages: List[Message]):
         await asyncio.gather(
--- notifier/message_consumers/chat_types/telegram_tests.py
+++ notifier/message_consumers/chat_types/telegram_tests.py
@@ -1,21 +1,52 @@
+from unittest.mock import MagicMock
+
 import pytest
+import telebot
 from django.core.exceptions import ValidationError
+from telebot.apihelper import ApiException
+from telebot.types import Chat
 
 from .telegram import TelegramGroupChat
 
 
-@pytest.mark.skip(reason='Not realized')
-def test_validate_params():
+def test_validate_params(mocker):
+    chat_id = 123456
+
+    patched_telebot = MagicMock()
+    mocker.patch.object(telebot, 'AsyncTeleBot', return_value=patched_telebot)
+    mocked_chat = MagicMock()
+    patched_telebot.get_chat.return_value = mocked_chat
+    mocked_chat.wait.return_value = Chat(chat_id, None)
     TelegramGroupChat.validate_params(
-        {}
+        {
+            'bot_token': 'fake:token',
+            'chat_id': chat_id,
+        }
     )
 
 
-@pytest.mark.skip(reason='Not realized')
-def test_validate_params_should_raise_error():
+def test_validate_params_should_raise_validation_error(mocker):
     with pytest.raises(
         ValidationError,
+        match='chat not found'
     ):
+        patched_telebot = MagicMock()
+        mocker.patch.object(telebot, 'AsyncTeleBot', return_value=patched_telebot)
+        mocked_chat = MagicMock()
+        patched_telebot.get_chat.return_value = mocked_chat
+        mocked_chat.wait.return_value = (
+            ApiException,
+            ApiException(
+                'A request to the Telegram API was unsuccessful. '
+                'The server returned HTTP 400 Bad Request. Response body: '
+                '[b\'{"ok":false,"error_code":400,"description":"Bad Request: chat not found"}\']',
+                None, None,
+            ),
+            'traceback'
+        )
         TelegramGroupChat.validate_params(
-            {}
+            {
+                'bot_token': 'fake:token',
+                'chat_id': 123456789,
+            }
         )
