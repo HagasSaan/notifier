076089ad1b3eafe3b3e5ea7b949384d1d3cbb38a
Artem Konovalov <artemian000@gmail.com>
2020-08-24 01:59:51 +0700

    configuration tests looks like done

--- notifier/configuration/models/configuration_tests.py
+++ notifier/configuration/models/configuration_tests.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import Dict
+from typing import Dict, Tuple, List
 
 import freezegun
 import pytest
@@ -10,6 +10,7 @@ from configuration.factories import (
     SkipKeywordFactory,
     UserFactory,
 )
+from configuration.models import User
 from helpers.messages_components import Message
 from message_consumers.factories import TestConsumer
 from message_producers.factories import TestProducer
@@ -35,11 +36,8 @@ def test_run_configuration_raises_error_if_consumer_or_producer_not_specified(
         configuration.run()
 
 
-@freezegun.freeze_time('15:00:00')
-def test_run_configuration(
-    db: MockFixture,
-    mocker: MockFixture,
-) -> None:
+@pytest.fixture
+def setup(db: MockFixture) -> Tuple[User, User, List[Message]]:
     user1 = UserFactory(
         on_leave=False,
         working_time_start=datetime.time(8, 0, 0),
@@ -50,16 +48,22 @@ def test_run_configuration(
         working_time_start=datetime.time(8, 0, 0),
         working_time_end=datetime.time(17, 0, 0),
     )
-    user_not_working = UserFactory(
-        on_leave=True,
-        working_time_start=datetime.time(8, 0, 0),
-        working_time_end=datetime.time(17, 0, 0),
-    )
-    user_not_in_config = UserFactory(
-        on_leave=False,
-        working_time_start=datetime.time(8, 0, 0),
-        working_time_end=datetime.time(17, 0, 0),
-    )
+
+    messages_should_be_consumed = [
+        Message(user1.username, user2.username, 'message1'),
+        Message(user2.username, user1.username, 'message2'),
+    ]
+
+    return user1, user2, messages_should_be_consumed
+
+
+@freezegun.freeze_time('15:00:00')
+def test_run_configuration_should_filter_message_where_receiver_doesnt_have_consumer_username(
+    db: MockFixture,
+    mocker: MockFixture,
+    setup: Tuple[User, User, List[Message]],
+) -> None:
+    user1, user2, messages_should_be_consumed = setup
     user_without_consumer_username = UserFactory(
         on_leave=False,
         working_time_start=datetime.time(8, 0, 0),
@@ -68,18 +72,12 @@ def test_run_configuration(
     user_without_consumer_username.additional_info.pop(TestConsumer.username_key)
     user_without_consumer_username.save()
 
-    skip_keyword = SkipKeywordFactory()
-
     messages_should_be_consumed = [
         Message(user1.username, user2.username, 'message1'),
         Message(user2.username, user1.username, 'message2'),
     ]
 
     fake_messages = messages_should_be_consumed + [
-        Message(user2.username, user_not_working.username, 'message to not working user'),
-        Message(user2.username, user1.username, f'message with {skip_keyword.word}'),
-        Message(user2.username, user_not_in_config.username, 'messsage to not in config user'),
-        Message('unknown_user', user1.username, 'message from unknown user'),
         Message(
             user1.username,
             user_without_consumer_username.username,
@@ -92,10 +90,118 @@ def test_run_configuration(
     consume_messages_spy = mocker.spy(TestConsumer, 'consume_messages')
 
     configuration = ConfigurationFactory(
-        users=(user1, user2, user_not_working, user_without_consumer_username),
+        users=(user1, user2, user_without_consumer_username),
+    )
+
+    configuration.run()
+
+    assert consume_messages_spy.call_args.args[1] == messages_should_be_consumed
+
+
+@freezegun.freeze_time('15:00:00')
+def test_run_configuration_should_filter_message_where_receiver_is_not_working(
+    db: MockFixture,
+    mocker: MockFixture,
+    setup: Tuple[User, User, List[Message]],
+) -> None:
+    user1, user2, messages_should_be_consumed = setup
+    user_not_working = UserFactory(
+        on_leave=True,
+        working_time_start=datetime.time(8, 0, 0),
+        working_time_end=datetime.time(17, 0, 0),
+    )
+
+    fake_messages = messages_should_be_consumed + [
+        Message(user2.username, user_not_working.username, 'message to not working user'),
+    ]
+
+    mocker.patch.object(TestProducer, 'produce_messages', return_value=fake_messages)
+
+    consume_messages_spy = mocker.spy(TestConsumer, 'consume_messages')
+
+    configuration = ConfigurationFactory(
+        users=(user1, user2, user_not_working),
+    )
+
+    configuration.run()
+
+    assert consume_messages_spy.call_args.args[1] == messages_should_be_consumed
+
+
+@freezegun.freeze_time('15:00:00')
+def test_run_configuration_should_filter_message_with_skip_keywords(
+    db: MockFixture,
+    mocker: MockFixture,
+    setup: Tuple[User, User, List[Message]],
+) -> None:
+    user1, user2, messages_should_be_consumed = setup
+
+    skip_keyword = SkipKeywordFactory()
+
+    fake_messages = messages_should_be_consumed + [
+        Message(user2.username, user1.username, f'message with {skip_keyword.word}'),
+    ]
+
+    mocker.patch.object(TestProducer, 'produce_messages', return_value=fake_messages)
+
+    consume_messages_spy = mocker.spy(TestConsumer, 'consume_messages')
+
+    configuration = ConfigurationFactory(
+        users=(user1, user2),
         skip_keywords=(skip_keyword,),
     )
 
+    configuration.run()
+
+    assert consume_messages_spy.call_args.args[1] == messages_should_be_consumed
+
+
+@freezegun.freeze_time('15:00:00')
+def test_run_configuration_should_filter_message_if_user_not_in_config(
+    db: MockFixture,
+    mocker: MockFixture,
+    setup: Tuple[User, User, List[Message]],
+) -> None:
+    user1, user2, messages_should_be_consumed = setup
+    user_not_in_config = UserFactory(
+        on_leave=False,
+        working_time_start=datetime.time(8, 0, 0),
+        working_time_end=datetime.time(17, 0, 0),
+    )
+
+    fake_messages = messages_should_be_consumed + [
+        Message(user2.username, user_not_in_config.username, 'message to not in config user'),
+    ]
+
+    mocker.patch.object(TestProducer, 'produce_messages', return_value=fake_messages)
+
+    consume_messages_spy = mocker.spy(TestConsumer, 'consume_messages')
+
+    configuration = ConfigurationFactory(
+        users=(user1, user2),
+    )
+
+    configuration.run()
+
+    assert consume_messages_spy.call_args.args[1] == messages_should_be_consumed
+
+
+@freezegun.freeze_time('15:00:00')
+def test_run_configuration_should_filter_message_if_user_is_unknown(
+    db: MockFixture,
+    mocker: MockFixture,
+    setup: Tuple[User, User, List[Message]],
+) -> None:
+    user1, user2, messages_should_be_consumed = setup
+
+    fake_messages = messages_should_be_consumed + [
+        Message('unknown_user', user1.username, 'message from unknown user'),
+    ]
+
+    mocker.patch.object(TestProducer, 'produce_messages', return_value=fake_messages)
+    consume_messages_spy = mocker.spy(TestConsumer, 'consume_messages')
+
+    configuration = ConfigurationFactory(users=(user1, user2))
     configuration.run()
 
     assert consume_messages_spy.call_args.args[1] == messages_should_be_consumed
