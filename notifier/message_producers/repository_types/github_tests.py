cc79e522f51d871c53f9beab44b6aaac075f4d12
Artem Konovalov <artemian000@gmail.com>
2020-08-29 16:45:15 +0700

    github tests done

--- notifier/message_producers/repository_types/github_tests.py
+++ notifier/message_producers/repository_types/github_tests.py
@@ -1,6 +1,9 @@
+import os
+
 import pytest
 from aioresponses import aioresponses
 from django.core.exceptions import ValidationError
+from pytest_mock import MockFixture
 
 from helpers.messages_components import Message
 from message_producers.repository_types.github import GithubRepository
@@ -14,61 +17,80 @@ def f_github_repository() -> GithubRepository:
     )
 
 
-@pytest.mark.asyncio
-async def test_github_get_pull_requests(f_github_repository: GithubRepository) -> None:
-    with aioresponses() as mocked, open('github_pulls_fixture.json', 'rb') as fixture:
+@pytest.fixture
+def m_pulls_fixture() -> bytes:
+    with open(
+        (
+            os.path.dirname(os.path.abspath(__file__)) +
+            '/github_pulls_fixture.json'
+        ), 'rb'
+    ) as fixture:
+        yield fixture.read()
+
+
+@pytest.fixture
+def m_responses(
+    f_github_repository: GithubRepository,
+    m_pulls_fixture: bytes,
+) -> MockFixture:
+    with aioresponses() as mocked:
         mocked.get(
             f_github_repository.URL_PULLS.format(repository=f_github_repository.name),
-            body=fixture.read(),
+            body=m_pulls_fixture,
         )
-        pull_requests = await f_github_repository.get_pull_requests()
-
-        assert len(pull_requests) == 1
-        pull_request = pull_requests[0]
-
-        assert pull_request.html_url == 'https://github.com/tekliner/dsas/pull/3045'
-        assert pull_request.title == 'IMD-8404: Facebook limit error'
-        assert pull_request.url == 'https://api.github.com/repos/tekliner/dsas/pulls/3045'
-        assert pull_request.updated_at == '2020-08-28T11:36:52Z'
-        assert (
-            len(pull_request.assignees) == 2
-            and {
-                assignee.login
-                for assignee in pull_request.assignees
-            } == {'vlmihnevich', '123phg'}
-        )
-        assert len(pull_request.labels) == 1 and pull_request.labels[0].name == 'Facebook'
+        yield mocked
 
 
 @pytest.mark.asyncio
-async def test_github_produce_messages(f_github_repository: GithubRepository) -> None:
-    with aioresponses() as mocked, open('github_pulls_fixture.json', 'rb') as fixture:
-        mocked.get(
-            f_github_repository.URL_PULLS.format(repository=f_github_repository.name),
-            body=fixture.read(),
-        )
+async def test_github_get_pull_requests(
+    f_github_repository: GithubRepository,
+    m_responses: MockFixture,
+) -> None:
+    pull_requests = await f_github_repository.get_pull_requests()
 
-        messages = await f_github_repository.produce_messages()
-        assert set(messages) == {
-            Message(
-                sender='comeuplater',
-                receiver='vlmihnevich',
-                content=(
-                    'IMD-8404: Facebook limit error '
-                    '(https://github.com/tekliner/dsas/pull/3045)\n'
-                    'Labels: Facebook'
-                )
-            ),
-            Message(
-                sender='comeuplater',
-                receiver='123phg',
-                content=(
-                    'IMD-8404: Facebook limit error '
-                    '(https://github.com/tekliner/dsas/pull/3045)\n'
-                    'Labels: Facebook'
-                )
+    assert len(pull_requests) == 1
+    pull_request = pull_requests[0]
+
+    assert pull_request.html_url == 'https://github.com/tekliner/dsas/pull/3045'
+    assert pull_request.title == 'IMD-8404: Facebook limit error'
+    assert pull_request.url == 'https://api.github.com/repos/tekliner/dsas/pulls/3045'
+    assert pull_request.updated_at == '2020-08-28T11:36:52Z'
+    assert (
+        len(pull_request.assignees) == 2
+        and {
+            assignee.login
+            for assignee in pull_request.assignees
+        } == {'vlmihnevich', '123phg'}
+    )
+    assert len(pull_request.labels) == 1 and pull_request.labels[0].name == 'Facebook'
+
+
+@pytest.mark.asyncio
+async def test_github_produce_messages(
+    f_github_repository: GithubRepository,
+    m_responses: MockFixture,
+) -> None:
+    messages = await f_github_repository.produce_messages()
+    assert set(messages) == {
+        Message(
+            sender='comeuplater',
+            receiver='vlmihnevich',
+            content=(
+                'IMD-8404: Facebook limit error '
+                '(https://github.com/tekliner/dsas/pull/3045)\n'
+                'Labels: Facebook'
             )
-        }
+        ),
+        Message(
+            sender='comeuplater',
+            receiver='123phg',
+            content=(
+                'IMD-8404: Facebook limit error '
+                '(https://github.com/tekliner/dsas/pull/3045)\n'
+                'Labels: Facebook'
+            )
+        )
+    }
 
 
 def test_validate_params() -> None:
