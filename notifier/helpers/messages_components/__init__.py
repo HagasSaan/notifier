c6bf51f88a32ce6950ab8e8252022a2b4aa85969
Artem Konovalov <artemian000@gmail.com>
2020-09-14 19:16:47 +0700

    refactor

--- notifier/helpers/messages_components/__init__.py
+++ notifier/helpers/messages_components/__init__.py
@@ -1,8 +1,5 @@
 from .message import Message
-from message_producers.producers.message_producer import MessageProducer, PRODUCER_REGISTRY_NAME
 
 __all__ = (
     'Message',
-    'MessageProducer',
-    'PRODUCER_REGISTRY_NAME',
 )
