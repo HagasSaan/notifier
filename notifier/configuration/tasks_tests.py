6f3bc01be38fc7398a8f7bff40165a6f2c892576
Artem Konovalov <artemian000@gmail.com>
2020-10-03 20:01:11 +0700

    tasks tests

--- notifier/configuration/tasks_tests.py
+++ notifier/configuration/tasks_tests.py
@@ -1,16 +1,32 @@
-import pytest
 from pytest_mock import MockFixture
 
 from .factories import ConfigurationFactory
+from .models import Configuration
 from .tasks import run_configuration
 
 
-@pytest.mark.skip('Not working yet')
-def test_run_configuration(db: MockFixture) -> None:
+def test_run_configuration(
+    db: MockFixture,
+    mocker: MockFixture,
+    capsys: MockFixture,
+) -> None:
+    mocker.patch.object(Configuration, 'run')
     configuration = ConfigurationFactory()
     run_configuration(configuration.id)
+    captured = capsys.readouterr()
+    assert 'Configuration executed' in captured.out
 
 
-@pytest.mark.skip('Not working yet')
-def test_run_configuration_logs_error() -> None:
-    run_configuration(42)
+def test_run_configuration_logs_error(
+    db: MockFixture,
+    mocker: MockFixture,
+    capsys: MockFixture,
+) -> None:
+    mocker.patch.object(
+        Configuration, 'run',
+        side_effect=Exception('error occurred')
+    )
+    configuration = ConfigurationFactory()
+    run_configuration(configuration.id)
+    captured = capsys.readouterr()
+    assert 'Configuration execution failed reason=error occurred' in captured.out
