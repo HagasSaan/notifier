354a0b05a66d3a22a6c617cc49e36643db4c7e0d
Artem Konovalov <artemian000@gmail.com>
2020-10-07 15:47:22 +0700

    run configuration as api action

--- notifier/configuration/views.py
+++ notifier/configuration/views.py
@@ -1,13 +1,30 @@
+from typing import Optional
+
+from django.conf import settings
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from .models import Configuration, User
 from .serializers import ConfigurationSerializer, UserSerializer
+from .tasks import run_configuration as run_configuration_task
 
 
 class ConfigurationViewSet(viewsets.ModelViewSet):
     queryset = Configuration.objects.all()
     serializer_class = ConfigurationSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=True, methods=['post'])
+    def run_configuration(self, request: Request, pk: Optional[str] = None):
+        configuration = Configuration.objects.get(pk=pk)
+        if settings.SYNC_MODE:
+            configuration.run()
+        else:
+            run_configuration_task.delay(configuration.id)
+
+        return Response({'status': 'success'})
 
 
 class UserViewSet(viewsets.ModelViewSet):
